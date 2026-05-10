33a67d7017d671b575d5fb5db27643c1bc4c372a
shri29s <shricharan.r24@iiits.in>
2026-05-10 18:09:30 +0530

    update version

--- app.py
+++ app.py
@@ -39,4 +39,4 @@ def predict():
         return render_template("index.html", error=f"An error occurred: {str(e)}")
 
 if __name__=="__main__":
-    app.run("0.0.0.0")
+    app.run("0.0.0.0", port=5000, debug=True)
